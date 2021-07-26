c0ec4f5c409f3e5b74feb8166623cbc547c1a00f
Vishnu <kvv.153@gmail.com>
2021-07-26 19:59:18 +0530

    Updated

--- MavenPracticeProject/src/test/java/tests/FirstProgram.java
+++ MavenPracticeProject/src/test/java/tests/FirstProgram.java
@@ -1,36 +1,46 @@
 package tests;
-	
-	import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-	import org.testng.annotations.Test;
-
+import org.openqa.selenium.opera.OperaDriver;
+import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 import org.testng.annotations.BeforeClass;
-	import org.testng.annotations.AfterClass;
-	 
-	public class FirstProgram {
-	 
-	 WebDriver driver;
-	 
-	  @Test
-	  public void openMyBlog() throws Exception {
-	 driver.get("https://www.softwaretestingmaterial.com/");
-	 Thread.sleep(6000);
-	  }
-	  
-	  @BeforeClass
-	  public void beforeClass() {
-	   
-		  WebDriverManager.chromedriver().setup();
-		  driver = new ChromeDriver();
-		  
-	  }
-	 
-	  @AfterClass
-	  public void afterClass() {
-	   driver.quit();
-	  }
-	 
+import org.testng.annotations.AfterClass;
+
+public class FirstProgram {
+
+	WebDriver driver;
+
+	@Test
+	public void openMyBlog() throws Exception {
+		driver.get("https://www.softwaretestingmaterial.com/");
+		Thread.sleep(6000);
 	}
+
+	@BeforeClass
+	public void beforeClass() {
+
+//		  WebDriverManager.chromedriver().setup();
+//		  driver = new ChromeDriver();
+//		  WebDriverManager.firefoxdriver().setup();
+//		  driver = new FirefoxDriver();
+
+		WebDriverManager.operadriver().setup();
+		driver = new OperaDriver();
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.manage().deleteAllCookies();
+		driver.quit();
+	}
+
+}
